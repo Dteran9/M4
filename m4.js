72fdc3a550cd9f1a2446d8084e17fa3b04cb9a60
Deanndra <97985953+Deanndra@users.noreply.github.com>
2023-09-18 13:07:45 -0500

    created js if else statements

---
+++ m4.js
@@ -0,0 +1,7 @@
+const age = 18;
+
+if(age < 21){ // age is smaller than 21
+    console.log("You are NOT old enough to drink!");
+} else {
+console.log("You are old enough! But always make safe choices.");
+}
