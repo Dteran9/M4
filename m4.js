e65339addfe3c73dd03395bf68fcad05752e5b1c
Deanndra <97985953+Deanndra@users.noreply.github.com>
2023-09-18 13:36:09 -0500

    created concatenated string variables

--- m4.js
+++ m4.js
@@ -1,7 +1,15 @@
 const age = 18;
 
 if(age < 21){ // age is smaller than 21
-    console.log("You are NOT old enough to drink!");
+    console.log("You are NOT old enough to drink Alcohol!");
 } else {
-console.log("You are old enough! But always make safe choices.");
+console.log("You are old enough to drink Alcohol! But always make safe choices.");
 }
+
+const myAge = 20;
+const myName = "Deanndra";
+const bfName = "Fernando";
+const bfAge = 23;
+
+console.log(`Hello my name is ${myName} and I am ${myAge} years old. Being that I am ${myAge}, this means I am not just yet old enough to drink alcohol.`);
+console.log(`My boyfriends name is ${bfName} and he is ${bfAge} years old. Being that he is ${bfAge}, this means that he is infact old enough to drink alcohol as long as he makes the right choices.`);
